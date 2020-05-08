cbdab611742d62c2cd8c952a4bd59e23c78efe77
Ave-Maria Nlandou <32993795+anlandou@users.noreply.github.com>
2020-05-08 01:14:23 -0400

    Update diceRoll.py

--- projects/diceRoll.py
+++ projects/diceRoll.py
@@ -1,1 +1,25 @@
-print("Hello World") 
+import random
+
+def main():
+  num = random.randint(1, 6)
+
+  print(num)
+
+  print("The number is", num)
+
+print(
+  "Would you like to roll the dice again? (Please type yes or no only)")
+
+x = input()
+
+yes = "yes"
+
+if (x == yes):
+    main()
+
+else :
+  print("Thanks for playing")
+
+return
+
+main()
